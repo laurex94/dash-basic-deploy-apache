e3fbfb7b8c6da3af38f6e3c302b90ccddb439224
Laurex94 <laurencejose94@gmail.com>
2022-02-09 10:46:34 -0400

    update file

--- dashing_demo_app.py
+++ dashing_demo_app.py
@@ -1,6 +1,6 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import  dcc
+from dash import html
 
 # for deployment, pass app.server (which is the actual flask app) to WSGI etc
 app = dash.Dash()
